40882eece2946b8902f298369a30301b952e2965
wim2 <wim2@aber.ac.uk>
2014-02-11 12:15:20 +0000

    Added skeleton FragmentActivity containing navigation side bar to join between different fragment pages.

--- StudentBudgetApplication/build/source/r/debug/uk/ac/aber/dcs/aber/studentbudgetapplication/R.java
+++ StudentBudgetApplication/build/source/r/debug/uk/ac/aber/dcs/aber/studentbudgetapplication/R.java
@@ -8,33 +8,49 @@
 package uk.ac.aber.dcs.aber.studentbudgetapplication;
 
 public final class R {
+    public static final class array {
+        /**  From: file:/aber/wim2/major%20project/StudentBudgetApplicationProject/StudentBudgetApplication/src/main/res/values/strings.xml 
+         */
+        public static final int items=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class dimen {
         /**  From: file:/aber/wim2/major%20project/StudentBudgetApplicationProject/StudentBudgetApplication/src/main/res/values/dimens.xml 
  From: file:/aber/wim2/major%20project/StudentBudgetApplicationProject/StudentBudgetApplication/src/main/res/values-sw720dp-land/dimens.xml 
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_drawer=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090005;
+        public static final int content_frame=0x7f090003;
+        public static final int drawer_layout=0x7f090002;
+        public static final int enterButton=0x7f090001;
+        public static final int left_drawer=0x7f090004;
+        public static final int welcomeText=0x7f090000;
     }
     public static final class layout {
         public static final int activity_enter=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int listview_item=0x7f030002;
     }
     public static final class menu {
-        public static final int enter=0x7f070000;
+        public static final int enter=0x7f080000;
+        public static final int main=0x7f080001;
     }
     public static final class string {
         /**  From: file:/aber/wim2/major%20project/StudentBudgetApplicationProject/StudentBudgetApplication/src/main/res/values/strings.xml 
          */
-        public static final int action_settings=0x7f050000;
-        public static final int app_name=0x7f050001;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060000;
+        public static final int app_name=0x7f060001;
+        public static final int drawer_close=0x7f060002;
+        public static final int drawer_open=0x7f060003;
+        public static final int title_activity_main=0x7f060004;
     }
     public static final class style {
         /**  From: file:/aber/wim2/major%20project/StudentBudgetApplicationProject/StudentBudgetApplication/src/main/res/values/styles.xml 
@@ -48,9 +64,9 @@ public final class R {
  From: file:/aber/wim2/major%20project/StudentBudgetApplicationProject/StudentBudgetApplication/src/main/res/values-v14/styles.xml 
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
---
+++ StudentBudgetApplication/src/main/java/uk/ac/aber/dcs/aber/studentbudgetapplication/MainActivity.java
@@ -0,0 +1,112 @@
+package uk.ac.aber.dcs.aber.studentbudgetapplication;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.app.Activity;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends FragmentActivity {
+    private String[] drawerListViewItems;
+    private ListView drawerListView;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // get list items from strings.xml
+        drawerListViewItems = getResources().getStringArray(R.array.items);
+
+        // get ListView defined in activity_main.xml
+        drawerListView = (ListView) findViewById(R.id.left_drawer);
+
+        // Set the adapter for the list view
+        drawerListView.setAdapter(new ArrayAdapter<String>(this,
+                R.layout.listview_item, drawerListViewItems));
+
+        // App Icon
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                drawerLayout,         /* DrawerLayout object */
+                R.drawable.ic_drawer,  /* nav drawer icon to replace 'Up' caret */
+                R.string.drawer_open,  /* "open drawer" description */
+                R.string.drawer_close  /* "close drawer" description */
+        );
+
+        // Set actionBarDrawerToggle as the DrawerListener
+        drawerLayout.setDrawerListener(actionBarDrawerToggle);
+
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        drawerListView.setOnItemClickListener(new DrawerItemClickListener());
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        actionBarDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        // call ActionBarDrawerToggle.onOptionsItemSelected(), if it returns true
+        // then it has handled the app icon touch event
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        actionBarDrawerToggle.syncState();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView parent, View view, int position, long id) {
+            Toast.makeText(MainActivity.this, ((TextView) view).getText(), Toast.LENGTH_LONG).show();
+
+            if(position == 0){
+//                Fragment frag = new ExampleActivity();
+//                FragmentManager fragmentManager = getFragmentManager();
+//                fragmentManager.beginTransaction()
+//                        .replace(R.id.content_frame, frag)
+//                        .commit();
+
+            }
+
+            drawerLayout.closeDrawer(drawerListView);
+
+
+
+        }
+    }
+    
+}
