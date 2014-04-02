3ac700b12d1f75889980860cc7306916d09445d7
wim2 <wim2@aber.ac.uk>
2014-04-02 11:02:05 +0100

    - Fixed bug where overview budget was calculated wrong

--- StudentBudgetApplication/src/main/java/uk/ac/aber/dcs/wim2/studentbudgetapplication/fragments/OverviewFragment.java
+++ StudentBudgetApplication/src/main/java/uk/ac/aber/dcs/wim2/studentbudgetapplication/fragments/OverviewFragment.java
@@ -10,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -50,8 +51,8 @@ public class OverviewFragment extends ListFragment {
     }
 
     private void registerViews(View view) {
-        weeklyIncome = (TextView) view.findViewById(R.id.overWeeklyIncome);
-        weeklyExpense = (TextView) view.findViewById(R.id.overWeeklyExpense);
+//        weeklyIncome = (TextView) view.findViewById(R.id.overWeeklyIncome);
+//        weeklyExpense = (TextView) view.findViewById(R.id.overWeeklyExpense);
         weeklyBalance = (TextView) view.findViewById(R.id.overWeeklyBalance);
         list = (ListView)view.findViewById(android.R.id.list);
 
@@ -73,8 +74,11 @@ public class OverviewFragment extends ListFragment {
 
     private void setup() {
         currency = FragmentUtilities.getCurrency(getActivity());
-        weeklyIncome.setText(currency+BalanceUtilities.getValueAs2dpString(detail.getWeeklyIncome()));
-        weeklyExpense.setText(currency+BalanceUtilities.getValueAs2dpString(detail.getWeeklyExpense()));
+//        weeklyIncome.setText(currency+BalanceUtilities.getValueAs2dpString(detail.getWeeklyIncome()));
+//        weeklyExpense.setText(currency+BalanceUtilities.getValueAs2dpString(detail.getWeeklyExpense()));
+
+        setStartAndEndDates();
+
 
         detail = BalanceUtilities.recalculateBalance(detail, db);
         weeklyBalance.setText(currency+BalanceUtilities.getValueAs2dpString(detail.getWeeklyBalance()));
@@ -87,7 +91,61 @@ public class OverviewFragment extends ListFragment {
         list.setOnItemLongClickListener(listen);
         list.setOnItemClickListener(listen);
 
+        checkMainBudget();
+
         setListAdapter(listAdapter);
+    }
+
+    private void checkMainBudget() {
+        TextView overviewPercent = (TextView)getActivity().findViewById(R.id.overviewBudgetPercent);
+        ProgressBar overviewProgress = (ProgressBar)getActivity().findViewById(R.id.overviewProgressBar);
+
+        Calendar cal = Calendar.getInstance();
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        int month = cal.get(Calendar.MONTH);
+        int year = cal.get(Calendar.YEAR);
+        DateTime today = new DateTime(year, month+1, day, 0, 0);
+
+        String[] endSplit = detail.getStartDate().split("/");
+        DateTime startDate = new DateTime(Integer.valueOf(endSplit[2]), Integer.parseInt(endSplit[1]), Integer.valueOf(endSplit[0]), 0, 0);
+
+        float percent = ((Days.daysBetween(startDate, today).getDays()+1) * 100)/Float.valueOf(detail.getTotalWeeks());
+
+        overviewPercent.setText(BalanceUtilities.getValueAs2dpString(percent)+"%");
+        overviewProgress.setProgress((int)percent);
+
+
+    }
+
+    private void setStartAndEndDates() {
+        TextView weekStart = (TextView)getActivity().findViewById(R.id.overWeekStart);
+        TextView weekEnd = (TextView)getActivity().findViewById(R.id.overWeekEnd);
+
+        Calendar cal = Calendar.getInstance();
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        int month = cal.get(Calendar.MONTH);
+        int year = cal.get(Calendar.YEAR);
+        DateTime today = new DateTime(year, month+1, day, 0, 0);
+        today = today.withDayOfWeek(1);
+
+        String[] splitStart = detail.getStartDate().split("/");
+        DateTime startDate = new DateTime(Integer.valueOf(splitStart[2]), Integer.parseInt(splitStart[1]), Integer.valueOf(splitStart[0]), 0, 0);
+        if(startDate.isAfter(today)){
+            weekStart.setText(detail.getStartDate()+"  ");
+        }
+        else{
+            weekStart.setText(today.getDayOfMonth()+"/"+today.getMonthOfYear()+"/"+today.getYear()+"  ");
+        }
+
+        today = today.withDayOfWeek(7);
+        String[] splitEnd = detail.getEndDate().split("/");
+        DateTime endDate = new DateTime(Integer.valueOf(splitEnd[2]), Integer.parseInt(splitEnd[1]), Integer.valueOf(splitEnd[0]), 0, 0);
+        if(endDate.isBefore(today)){
+            weekEnd.setText("  "+detail.getEndDate());
+        }
+        else{
+            weekEnd.setText("  "+today.getDayOfMonth()+"/"+today.getMonthOfYear()+"/"+today.getYear());
+        }
     }
 
     private void checkBudgets() {
